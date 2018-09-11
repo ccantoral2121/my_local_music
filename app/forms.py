19d2381d778efb6193ecc49c283c2b319b770eca
ccantoral2121 <cristianggerardo@hotmail.com>
2018-09-11 19:40:31 -0400

    Update fix hard coded instances

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class NewArtistForm(FlaskForm):
+        name = StringField('Name', validators=[DataRequired()])
+        hometown = StringField('Hometown', validators=[DataRequired()])
+        description = TextAreaField('Description',validators=[DataRequired()])
+        submit = SubmitField('Create New Artist')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import NewArtistForm
 
 
 
@@ -7,7 +8,7 @@ from app import app
 @app.route('/index.html')
 def index():
 
-    return render_template('index.html')
+    return render_template('index.html',title='Home')
 
 @app.route('/list_artists.html')
 def list_artists():
@@ -15,16 +16,26 @@ def list_artists():
 
     list = ['Eminem','Kaney West','MGK']
 
-    return render_template('list_artists.html',list=list)
 
-@app.route('/create.html')
-def create_new_artist():
+    return render_template('list_artists.html',list=list,title='List of Artist')
 
-    return render_template('create.html')
 
-@app.route('/mockup.html')
-def mockup():
-    return render_template('mockup.html')
+@app.route('/add.html')
+def add_artist():
+
+    form = NewArtistForm()
+    return render_template('add.html', title='Create New Artist', form=form)
+
+
+@app.route('/artist.html')
+def artist_page():
+    artist = {'name': 'Eminem',
+              'hometown': 'St. Joseph, Missouri, U.S.',
+              'description': 'Marshall Bruce Mathers III (born October 17, 1972), known professionally as Eminem (often stylized as EMINƎM) is an American rapper, songwriter, record producer, record executive, and actor.',
+              'events': ['Saturday 15 September 2018, England','Sunday 16 September 2018, Munich']
+              }
+
+    return render_template('artist.html', artist=artist)
 
 if __name__ == '__main__':
     app.run()
